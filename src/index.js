399dfe2930c3ea71af918617da58dd345c426174
chiabi <chiabi88@gmail.com>
2018-05-23 13:06:28 +0900

    게시글 페이지 추가

--- src/index.js
+++ src/index.js
@@ -9,9 +9,14 @@ import axios from 'axios';
 const rootEl = document.querySelector('.root');
 const templates = {
   postList: document.querySelector('#post-list').content,
-  postItem: document.querySelector('#post-item').content
+  postItem: document.querySelector('#post-item').content,
+  postContent: document.querySelector('#post-content').content
 }
 
+function render(fragment) {
+  rootEl.textContent = '';
+  rootEl.appendChild(fragment);
+} 
 async function indexPage() {
   // 2. async, await
   // 원래 then()으로 기다리던 것을 await 뒤에 써주면 됨
@@ -29,14 +34,31 @@ async function indexPage() {
     const fragment = document.importNode(templates.postItem, true);
     const pEl = fragment.querySelector('.post-item__title');
     pEl.textContent = post.title;
+    pEl.addEventListener('click', e => {
+      postContentPage(post.id);
+    });
     listFragment.querySelector('.post-list').appendChild(fragment);
   });
-  rootEl.appendChild(listFragment);
-
+  render(listFragment);
+  
   // 1. Promise, then()
   // axios.get('http://localhost:3000/posts').then(res => {
-  //   
-  // });
+    //   
+    // });
+  }
+  
+async function postContentPage(postId) {
+  const res = await axios.get(`http://localhost:3000/posts/${postId}`);
+  const fragment = document.importNode(templates.postContent, true);
+  fragment.querySelector(`.post-content__title`).textContent = res.data.title;
+  fragment.querySelector(`.post-content__body`).textContent = res.data.body;
+
+  // 뒤로가기
+  fragment.querySelector(`.post-content__back-btn`).addEventListener('click', e => {
+    indexPage();
+  });
+  render(fragment);
 }
 
 indexPage();
+// postContentPage(1);
